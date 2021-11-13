97b12caa99d49e54e1f2161019b883835299ea39
zjy <1847899575@qq.com>
2021-11-13 17:22:18 +0800

    master commit2

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git!2");
         System.out.println("hello git!3");
         System.out.println("hello git!  hot-fix");
+        System.out.println("hello git!  master");
     }
 }
